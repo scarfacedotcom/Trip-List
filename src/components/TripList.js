1a247cc180346666ad484589f65d7f1da6e038df
scarfacedotcom <peteradaaku@gmail.com>
2022-07-22 18:49:42 +0100

    trip list component

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -1,12 +1,28 @@
+import { useState, useEffect } from "react"
+
+import './TripList.css'
+
 export default function TripList() {
+  const [ trips, setTrips ] = useState([])
 
-  fetch('http://localhost:3000/trips')
-    .then(res => res.json())
-    .then(json => console.log(json))
-
+  useEffect(() => {
+    fetch('http://localhost:3000/trips')
+      .then(res => res.json())
+       .then(json => setTrips(json))
+  }, [])
+  
   return (
-    <div>
+    <div className="trip-list">
       <h2>Trip List</h2>
+      <ul>
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <h3>{trip.title}</h3>
+            <p>{trip.price}</p>
+          </li>
+        ))}
+      </ul>
+      
     </div>
   )
 }
