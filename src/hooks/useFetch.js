59167741d1da4aa36e1532342cb0a9dbc822c848
scarfacedotcom <peteradaaku@gmail.com>
2022-07-23 06:14:13 +0100

    addded pending state

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -3,11 +3,16 @@ import { useState, useEffect } from "react"
 
 export const useFetch = (url) => {
  const [ data , setData ] = useState(null)
+ const [ isPending, setIsPending ] = useState(false)
 
  useEffect(() => {
   const fetchData = async () => {
+    setIsPending(true)
+
     const res = await fetch(url)
     const json = await res.json()
+
+    setIsPending(false)
     setData(json)
   }
 
@@ -15,6 +20,6 @@ export const useFetch = (url) => {
 
  }, [url])
 
- return{data}
+ return{data, isPending}
 
 }
