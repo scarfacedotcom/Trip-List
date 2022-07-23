634c0e38b600854fbd1f002bb007c340497c0c2a
scarfacedotcom <peteradaaku@gmail.com>
2022-07-23 06:43:21 +0100

    err message

--- src/components/TripList.js
+++ src/components/TripList.js
@@ -5,12 +5,13 @@ import './TripList.css'
 
 export default function TripList() {
   const [ url, setUrl ] = useState('http://localhost:3000/trips')
-  const { data:trips, isPending } = useFetch(url)
+  const { data:trips, isPending, error } = useFetch(url)
 
   return (
     <div className="trip-list">
       <h2>Trip List</h2>
       { isPending  && <div>Loading Trips....</div> }
+      { error && <div>{error}</div> }
       <ul>
         {trips && trips.map(trip => (
           <li key={trip.id}>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,22 +4,32 @@ import { useState, useEffect } from "react"
 export const useFetch = (url) => {
  const [ data , setData ] = useState(null)
  const [ isPending, setIsPending ] = useState(false)
+ const [ error, setError ] = useState(null)
 
  useEffect(() => {
   const fetchData = async () => {
     setIsPending(true)
 
-    const res = await fetch(url)
-    const json = await res.json()
+    try {
+      const res = await fetch(url)
+      if (!res.ok) {
+        throw new Error (res.statusText)
+      }
+      const json = await res.json()
 
-    setIsPending(false)
-    setData(json)
+      setIsPending(false)
+      setData(json)
+      setError(null)
+    } catch (err) {
+      setIsPending(false)
+      setError('could not fetch Data')
+    }
   }
 
   fetchData()
 
  }, [url])
 
- return{data, isPending}
+ return{data, isPending, error}
 
 }
